7d18f5956dd18a4e96b124c9aae52e436bc6ceb7
jongkodingabi <abisamhazim04@gmail.com>
2025-05-14 16:48:21 +0700

    changin profil photo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,13 @@
 import Profile from "./Profile";
 import Verse from "./Verse";
-import Photo from "../assets/Photo.png";
+import Me from "../assets/me.jpg";
 
 function Hero() {
   return (
     <div className="relative">
       <div className="absolute top-0 left-0 w-full h-60 -mt-10 bg-[url('/src/assets/HeroBackgroundImage.png')] bg-cover bg-center bg-no-repeat rounded-2xl"></div>
       <div className="relative z-10 px-5 pt-8 flex flex-col gap-8">
-        <Profile greeting="Good Morning" name="Mareta Ayu 👋🏻 " avatar={Photo} />
+        <Profile greeting="Good Morning" name="Abisam 👋🏻 " avatar={Me} />
         <Verse
           verse="“He said: ‘Therein you shall live, and therein you shall die, and from it you shall be brought out (i.e. resurrected).’”"
           surah="Al-A’raaf (7:25)"
