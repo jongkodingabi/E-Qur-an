37b30b078da79afe5aef4ea7eb163b08f0ecef7f
jongkodingabi <abisamhazim04@gmail.com>
2025-05-14 16:55:41 +0700

    changing profil

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,13 @@
 type ProfileProps = {
   greeting: string;
-  name: string;
-  avatar: string;
 };
 
-function Profile({ greeting, name, avatar }: ProfileProps) {
+function Profile({ greeting }: ProfileProps) {
   return (
     <div className="flex justify-between items-center">
       <div>
-        <p className="text-left text-sm font-light">{greeting}</p>
-        <p className="text-left text-base font-bold md:text-xl">{name}</p>
+        <p className="text-left text-xl font-bold">{greeting}</p>
       </div>
-      <img src={avatar} alt="Profile User" className="w-10 h-10" />
     </div>
   );
 }
